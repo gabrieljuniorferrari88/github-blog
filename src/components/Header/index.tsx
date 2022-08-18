0b7bacc5548eaf375ac40c50874c94c681584ea2
Gabriel Ferrari <gabrieljuniorferrari88@gmail.com>
2022-08-18 06:56:44 -0300

    fix: logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
+import Image from 'next/image';
 import * as S from './styles';
+
+import logo from '../../../public/Logo.svg';
 
 export function Header() {
   return (
     <S.HeaderContainer>
-      <img src="/logo.svg" alt="GitHub Blog" />
+      {/* <img src="/logo.svg" alt="GitHub Blog" /> */}
+      <Image src={logo} alt="GitHub Logo" />
     </S.HeaderContainer>
   );
 }
